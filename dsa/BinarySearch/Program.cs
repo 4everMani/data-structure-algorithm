1897b7fb3b72ab20594ea01dd798c8f4ebb9716e
4evermani <jaiswal.manish49@gmail.com>
2024-04-04 05:06:02 +0530

    added more binary search questions

--- dsa/BinarySearch/Program.cs
+++ dsa/BinarySearch/Program.cs
@@ -10,4 +10,6 @@
 
 // NoOfOccurances.Solution([2, 4, 6, 8, 8, 8, 11, 13], 8);
 
-SearchInSortedRotatedArray.Solution([5,1,3], 5);
+// SearchInSortedRotatedArray.Solution([5,1,3], 5);
+
+SingleElementInASortedArray.Solution([1,1,2,2,3,3,4,4,6,6,7,8,8]);
---
+++ dsa/BinarySearch/SingleElementInASortedArray.cs
@@ -0,0 +1,50 @@
+﻿
+namespace BinarySearch;
+
+public class SingleElementInASortedArray
+{
+    // You are given a sorted array ‘arr’ of ‘n’ numbers such that every number occurred twice in the array except one, which appears only once.
+    // Return the number that appears once.
+    // Example:
+    // Input: 'arr' = [1,1,2,2,4,5,5]
+    // Output: 4 
+    // Explanation: 
+    // Number 4 only appears once the array.
+
+    public static void Solution(int[] nums)
+    {
+        int result = FindUnique(nums);
+        System.Console.WriteLine(result);
+    }
+
+    private static int FindUnique(int[] nums)
+    {
+        if (nums.Length == 1) return nums[0];
+        else if (nums[0] != nums[1]) return nums[0];
+        else if (nums[^1] != nums[^2]) return nums[^1];
+        else
+        {  
+            int start = 1;
+            int end = nums.Length - 2;
+
+            while(start <= end)
+            {
+                int mid = start + (end - start) / 2;
+                if (nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1])
+                {
+                    return nums[mid];
+                }
+                else if (mid % 2 == 0 && nums[mid] == nums[mid + 1] || (mid % 2 == 1 && nums[mid] == nums[mid - 1]))
+                {
+                    start = mid + 1; 
+                }
+                else
+                { 
+                    end = mid - 1; 
+                }
+            }
+            return -1;
+        }
+    }  
+
+}
