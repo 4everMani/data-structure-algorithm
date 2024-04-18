f5734b8cc98c95bb45057c704216619dcd760088
4evermani <jaiswal.manish49@gmail.com>
2024-04-18 07:10:29 +0530

    added search in 2D matrix II problem

---
+++ dsa/BinarySearch/SearchIn2DMatrixII.cs
@@ -0,0 +1,27 @@
+﻿namespace BinarySearch;
+
+public class SearchIn2DMatrixII
+{
+
+    public static void Solution(int[][] matrix, int target)
+    {
+        bool result = SearchMatrix(matrix, target);
+        System.Console.WriteLine(result);
+    }
+
+     public static bool SearchMatrix(int[][] matrix, int target) {
+        int n = matrix.Length;
+        int m = matrix[0].Length;
+        if (target > matrix[n - 1][m - 1] || target < matrix[0][0]) return false;
+
+        int row = 0;
+        int col = m - 1;
+        while(row < n && col >= 0)
+        {
+            if (matrix[row][col] == target) return true;
+            else if(target < matrix[row][col]) col--;
+            else row++;
+        }
+        return false;
+    }
+}
