87c80030863dc5006532d3b0741becea0c0e957a
4evermani <jaiswal.manish49@gmail.com>
2024-05-21 07:23:21 +0530

    added basics recursion problem

---
+++ dsa/Recursion/Basics.cs
@@ -0,0 +1,23 @@
+﻿namespace Recursion;
+
+public class Basics
+{
+    public static void Run()
+    {
+        Func2(5);
+    }
+
+    public static void Func(int n)
+    {
+        if (n == 0) return;
+        Func(n - 1);
+        System.Console.WriteLine(n);
+    }
+
+    public static void Func2(int n)
+    {
+        if (n == 0) return;
+        Func2(n - 1);
+        System.Console.WriteLine(n);
+    }
+}
